73a89d9795573a77b81f32e931a75d73fe32eefb
Yesica <ymrodriguez@unah.hn>
2021-04-12 16:55:52 -0600

    Componentes navbar,slider

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { APP_BOOTSTRAP_LISTENER, Component } from '@angular/core';
+import {faPaw} from '@fortawesome/free-solid-svg-icons'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'zooshop';
-}
+
+
+ export class AppComponent  {
+   title = 'zooshop';
+
+   fapaw=faPaw
+
+
+ }
 
 var mycarusel=document.querySelector('#carrusel')
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './components/login/login.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SliderComponent } from './components/slider/slider.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
